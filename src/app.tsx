8a0419bef62e18e0fb15ee50716c5f943a7b4617
Ryan G <exogenesis@protonmail.com>
2025-08-11 17:32:25 +1000

    Apply Sidebar Layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,30 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import "./app.css";
+import { LayoutContextProvider } from "./components/Layout/LayoutContext";
+import NavbarContent from "./components/Layout/views/Navbar";
+import SidebarContent from "./components/Layout/views/SidebarContent";
+import BottomDash from "./components/Layout/views/BottomDash";
+import Layout from "./components/Layout/Layout";
+import Article from "./views/Article";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
+          <Suspense>
+            <LayoutContextProvider>
+              <Layout
+                NavbarContent={<NavbarContent />}
+                sidebarContent={<SidebarContent />}
+                bottomDash={<BottomDash mobileOnly={true} />}
+              >
+                {props.children}
+              </Layout>
+            </LayoutContextProvider>
+          </Suspense>
         </>
       )}
     >
